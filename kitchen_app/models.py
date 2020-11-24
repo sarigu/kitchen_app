30f6f3736f844f197c6d7258329b7cea59567422
Sari <sariguci@Saris-MacBook-Pro.local>
2020-11-24 18:46:47 +0100

    user can view list of their rooms

--- kitchen_app/models.py
+++ kitchen_app/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.forms import ModelForm
+from django.db.models.query import QuerySet
 
 
 class Room (models.Model):
--- kitchen_app/views.py
+++ kitchen_app/views.py
@@ -7,7 +7,17 @@ from .models import Room, RoomForm, RoomMembers
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'kitchen_app/index.html')
+    rooms = []
+    queryset = RoomMembers.objects.filter(userID=request.user).values_list('roomID', flat=True)
+    for roomID in queryset:
+        room = get_object_or_404(Room, pk=roomID)
+        rooms.append(room)
+
+    context={
+        'rooms': rooms,
+        'user': request.user,    
+    }
+    return render(request, 'kitchen_app/index.html', context)
 
 @login_required
 def create_room(request):
@@ -17,4 +27,15 @@ def create_room(request):
             newRoom = form.save()
             if(newRoom):
                 RoomMembers.create(request.user, newRoom, "admin")
-    return render(request, 'kitchen_app/dashboard.html')
+             
+    rooms = []
+    queryset = RoomMembers.objects.filter(userID=request.user).values_list('roomID', flat=True)
+    for roomID in queryset:
+        room = get_object_or_404(Room, pk=roomID)
+        rooms.append(room)
+        
+    context={
+        'rooms': rooms,
+        'user': request.user,    
+    }
+    return render(request, 'kitchen_app/index.html', context)
