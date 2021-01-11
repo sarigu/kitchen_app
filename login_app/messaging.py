7af50aab2e72344f9e7b186c25f4ceb157946f14
Sari <sariguci@Saris-MacBook-Pro.local>
2021-01-11 02:04:10 +0100

    added sign up confirmation email

--- login_app/messaging.py
+++ login_app/messaging.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 
-def email_message(message_dict):
+def email_password_reset(message_dict):
    contents = f"""
    Hi you, 
    Your token is: {message_dict['token']}
@@ -9,6 +9,23 @@ def email_message(message_dict):
    """
    send_mail(
       'Password Reset Token',
+      contents,
+      'sarigucki@gmail.com',
+      [message_dict['email']],
+      fail_silently=False
+   )
+
+def email_account_confirmation(message_dict):
+   contents = f"""
+   Hi welcome, 
+   Thank you for signing up. 
+   You can use your username: {message_dict['username']} and your password for logging in. 
+   You can create a room or wait to be invited to a room.
+   Make sure people in your kitchen know that you joined the app, so they can find and invite you.
+   See you soon! 
+   """
+   send_mail(
+      'Welcome',
       contents,
       'sarigucki@gmail.com',
       [message_dict['email']],
