c8010978446335f1913e505fa6438f64872088af
Sari <sariguci@Saris-MacBook-Pro.local>
2021-01-14 04:26:28 +0100

    added as asgi in routing.py

--- kitchen_app/routing.py
+++ kitchen_app/routing.py
@@ -3,6 +3,6 @@ from . import consumers
 from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path('notifications/', consumers.NotificationConsumer()),
+    path('notifications/', consumers.NotificationConsumer().as_asgi()),
     path('ws/chat/<str:room_name>/', ChatConsumer().as_asgi()),
 ]
