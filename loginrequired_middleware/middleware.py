3b8fe41f90ed8991dfb9d677a1767173763dcfcb
Sari <sariguci@Saris-MacBook-Pro.local>
2021-01-14 03:23:11 +0100

    updated middleware

--- loginrequired_middleware/middleware.py
+++ loginrequired_middleware/middleware.py
@@ -8,13 +8,13 @@ class LoginRequiredMiddleware:
         self.get_response = get_response
     
     def __call__(self, request):
-        response = self.get_response(request)
-        return response
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
         path = request.path_info.lstrip('/')
         # check if user is logged in
         if request.user.is_authenticated == False:  
             # check if url is in the list of excluded urls (urls everyone can access)
             if not path in settings.EXCLUDED_URLS:   
                 return redirect('login_app:login')
+
+        response = self.get_response(request)
+        return response
+
